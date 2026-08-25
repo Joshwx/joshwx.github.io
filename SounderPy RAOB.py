d3630003a8bea1a09e4998a4eca83ccd1587304b
sherm <jmsher10@louisville.edu>
2026-08-24 23:58:11 -0400

    updated for sounderpy

--- SounderPy RAOB.py
+++ SounderPy RAOB.py
@@ -3,13 +3,58 @@ import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
 import numpy as np
 import metpy
+import pandas as pd
+import datetime
+from datetime import datetime, timedelta
 
+#get utc time for the most recent one
+target_time = (pd.Timestamp.now(tz='UTC') - pd.Timedelta(minutes=60)).tz_localize(None).floor('h')
 
 site_id='BNA'
-year='2026'
-month='08'
-day='16'
-hour=12
+year=target_time.strftime('%Y')
+month=target_time.strftime('%m')
+day=target_time.strftime('%d')
+hour=target_time.strftime('%H')
+
+obs_data=spy.get_obs_data(site_id,year,month,day,hour)
+print('plotting...')
+sounding=spy.build_sounding(obs_data, color_blind=False,save=True, filename='sounding')
+
+plt.tight_layout()
+plt.show()
+
+site_id='ILN'
+year=target_time.strftime('%Y')
+month=target_time.strftime('%m')
+day=target_time.strftime('%d')
+hour=target_time.strftime('%H')
+
+obs_data=spy.get_obs_data(site_id,year,month,day,hour)
+print('plotting...')
+sounding=spy.build_sounding(obs_data, color_blind=False,save=True, filename='sounding')
+
+plt.tight_layout()
+plt.show()
+
+
+site_id='ILX'
+year=target_time.strftime('%Y')
+month=target_time.strftime('%m')
+day=target_time.strftime('%d')
+hour=target_time.strftime('%H')
+
+obs_data=spy.get_obs_data(site_id,year,month,day,hour)
+print('plotting...')
+sounding=spy.build_sounding(obs_data, color_blind=False,save=True, filename='sounding')
+
+plt.tight_layout()
+plt.show()
+
+site_id='SGF'
+year=target_time.strftime('%Y')
+month=target_time.strftime('%m')
+day=target_time.strftime('%d')
+hour=target_time.strftime('%H')
 
 obs_data=spy.get_obs_data(site_id,year,month,day,hour)
 print('plotting...')
